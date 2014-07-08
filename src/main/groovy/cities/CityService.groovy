5723736f76b7a9f680eb0c60143c31a3ab2d871e
geowarin <geowarin@gmail.com>
2014-07-08 21:02:42 +0200

    init

---
+++ src/main/groovy/cities/CityService.groovy
@@ -0,0 +1,13 @@
+package cities
+
+import org.springframework.stereotype.Service
+
+/**
+ *
+ * Date: 08/07/2014
+ * Time: 20:56
+ * @author Geoffroy Warin (http://geowarin.github.io)
+ */
+@Service
+class CityService {
+}
