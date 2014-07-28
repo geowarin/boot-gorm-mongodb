291b314bfdc00efe513b866a3dc7b0424829bc22
geowarin <geowarin@gmail.com>
2014-07-28 20:26:28 +0200

    Spock test !

--- src/test/groovy/cities/CityServiceTest.groovy
+++ src/test/groovy/cities/CityServiceTest.groovy
@@ -1,39 +1,28 @@
-//package cities
-//
-//import com.lordofthejars.nosqlunit.annotation.UsingDataSet
-//import com.mongodb.DB
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.test.context.ContextConfiguration
-//
-///**
-// *
-// * Date: 08/07/2014
-// * Time: 21:04
-// * @author Geoffroy Warin (http://geowarin.github.io)
-// */
-//@ContextConfiguration(classes = [Application])
-//@UsingDataSet
-//class CityServiceTest extends FongoSpec {
-//
-//    @Autowired
-//    CityService cityService
-//
-//    @Autowired
-//    DB db
-//
-//    def "Test gmongo"() {
-//        when:
-//        def cities = db.city.find()
-//
-//        then:
-//        cities.size() == 1
-//    }
-//
-////    def "Test"() {
-////        when:
-////        def cities = cityService.getAllCities()
-////
-////        then:
-////        cities.size() == 1
-////    }
-//}
+package cities
+
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+
+/**
+ *
+ * Date: 08/07/2014
+ * Time: 21:04
+ * @author Geoffroy Warin (http://geowarin.github.io)
+ */
+@ContextConfiguration(classes = [Application])
+@UsingDataSet
+class CityServiceTest extends FongoSpec {
+
+    @Autowired
+    CityService cityService
+
+    def "Test gmongo"() {
+        when:
+        def cities = City.findAll()
+
+        then:
+        cities.size() == 1
+        cities.name == ['Kikoo']
+    }
+}
